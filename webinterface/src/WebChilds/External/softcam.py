13d4163ea06fde08882edbaa4f380d0d02d7229a
toto <toto_golden@gmx.de>
2015-03-03 21:33:10 +0100

    Update softcam.py

--- webinterface/src/WebChilds/External/softcam.py
+++ webinterface/src/WebChilds/External/softcam.py
@@ -5,16 +5,15 @@ from Components.ConfigList import ConfigListScreen
 from Components.Label import Label
 from Components.Sources.List import List
 from Components.ScrollLabel import ScrollLabel
+from Components.About import about
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 from Tools.LoadPixmap import LoadPixmap
-
 from enigma import eTimer
 from os import path
 import os
 from Plugins.Extensions.Infopanel import Softcam
-
 import sys
 import struct
 import stat
@@ -25,6 +24,9 @@ from os import path as os_path, symlink, listdir, unlink, readlink, remove, syst
 from commands import getoutput
 from enigma import eConsoleAppContainer, gMainDC
 from Screens.Standby import TryQuitMainloop
+import socket
+import subprocess
+
 header_string = ''
 header_string += '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"'
 header_string += '"http://www.w3.org/TR/html4/loose.dtd">'
@@ -41,10 +43,11 @@ config.NFRSoftcam.waittime = ConfigSelection([('0',_("dont wait")),('1',_("1 sec
 config.NFRSoftcam.camdir = ConfigText(default = "/usr/emu", fixed_size=False)
 config.NFRSoftcam.camconfig = ConfigText(default = "/usr/keys", fixed_size=False)
 global infoList	
+global ownip
 def checkCams(objelt):
         infoList = []
         if os.path.isfile('/etc/emulist'):
-                print "emilist found"
+                print "emulist found"
         else:
                 os.system('echo "no Softcam installed" >/etc/emulist')        
         f = open("/etc/emulist", 'r')
@@ -56,32 +59,96 @@ def checkCams(objelt):
 		f.close()
        	infoList = zeilen	
         return infoList
+        
+
+def createip(objelt):
+	#self.iface = "eth0"
+	eth0 = about.getIfConfig('eth0')
+	if eth0.has_key('addr'):
+		ownip = eth0['addr']
+		#self.iface = 'eth0'
+
+	eth1 = about.getIfConfig('eth1')
+	if eth1.has_key('addr'):
+		ownip = eth1['addr']
+		#self.iface = 'eth1'
+
+	ra0 = about.getIfConfig('ra0')
+	if ra0.has_key('addr'):
+		ownip = ra0['addr']
+		#self.iface = 'ra0'
+
+	wlan0 = about.getIfConfig('wlan0')
+	if wlan0.has_key('addr'):
+		ownip = wlan0['addr']
+		#self.iface = 'wlan0'
+        return ownip
 
 class SoftcamControl(resource.Resource):
     
 	def render_GET(self, req):
+	        self.Console = Console()
 		global infoList
+		act_webif = False
+        	infoList = checkCams(None)
         	aktiv = '<input style="background-color&#58;#00b000;" type="button" value="Aktiviert">'
-        	aktivieren = '<input style="color&#58;#00b000;" type="submit" value="Aktivieren">'
+        	webaktivieren = '<input style="color&#58;#00b000;" type="submit" value="Open Cam Webif">'
+                aktivieren = '<input style="color&#58;#00b000;" type="submit" value="Aktivieren">'
         	deaktivieren = '<input style="color&#58;#FF0000;" type="submit" value="Deaktivieren">'
        		deaktiviert = '<input style="background-color&#58;#FF0000;" type="button" value="Deaktiviert">'
         	req.setResponseCode(http.OK)
         	req.setHeader('Content-type', 'text/html')
         	req.setHeader('charset', 'UTF-8')
+        	self.activecam = False
         	self.actcam = config.NFRSoftcam.actcam.value
-        	infoList = checkCams(None)
+        	camconfigs = []
+        	ps = subprocess.Popen(('ps', 'ax'), stdout=subprocess.PIPE)
+        	output = ps.communicate()[0]
+        	for line in output.split('\n'):
+            	    if self.actcam in line:
+                        t = line.find("keys")
+                        t1 = len(line)
+                        camconfigs.append(line[t:t1])
+                        self.activecam = True
+        	
+                if self.activecam == False:
+                    print "no Softcam Activ!"
+                    #self.actcam = "None"
+                else:
+                    if 'doscam' in self.actcam:
+                        searchfile = camconfigs[0] + '/doscam.cfg'
+                    elif 'oscam' in self.actcam: 
+                        searchfile = camconfigs[0] + '/oscam.conf' 
+                    elif 'cccam' in self.actcam or 'CCcam' in self.actcam: 
+                        searchfile = camconfigs[0]
+                    if 'oscam' in self.actcam or 'doscam' in self.actcam or 'cccam' in self.actcam or 'CCcam' in self.actcam:
+                        fdc = open('/usr/' + searchfile) 
+                        zeilencfg = fdc.read().split('\n')
+                        fdc.close()
+                        for line in zeilencfg:
+                            if line.startswith("WEBINFO") or line.startswith("httpport"):
+                                newline = line.rsplit(" ", 1)
+                                act_webif = True
         	camdinfo = ''
         	html = header_string
         	for info in infoList:
        		    addExternalChild(('%sstart' % info, CamdStart(info)))
         	    addExternalChild(('%skill' % info, CamdKill(info)))
         	    if info == self.actcam:
-        	    #if info[2] == 'yes':
-        	        camd = '%s<a href="%skill" target="_self">%s</a>' % (aktiv, info, deaktivieren)
-        	    else:
+        	        if 'oscam' in self.actcam or 'doscam' in self.actcam or 'cccam' in self.actcam or 'CCcam' in self.actcam:
+                            if act_webif == True:
+                                camd = '%s<a href="%skill" target="_self">%s</a>' % (aktiv, info, deaktivieren)
+                                #camd = '%s<a href="%skill" target="_self">%s</a></td><td align="right">%s_Webif<a href="http://%s:%s" target="_blank">%s</a>' % (aktiv, info, deaktivieren, self.actcam, ownip, newline[1], webaktivieren)
+                                html += '<center><table style="width: 80%%;table-layout: fixed;" border="1" cellspacing="0"><tr><td align="center">%s_Webif<a href="http://%s:%s" target="_blank">%s</a></td></tr></center>' % (self.actcam, ownip, newline[1], webaktivieren)
+                            else:
+                                camd = '%s<a href="%skill" target="_self">%s</a>' % (aktiv, info, deaktivieren)
+                        else:
+                            camd = '%s<a href="%skill" target="_self">%s</a>' % (aktiv, info, deaktivieren)
+        	    else:   
                 	camd = '<a href="%sstart" target="_self">%s</a>%s</form>' % (info, aktivieren, deaktiviert)
         	    html += '<center><table style="width: 80%%;table-layout: fixed;" border="1" cellspacing="0"><tr><td align="left">%s:</td><td align="right">%s</td></tr></center>' % (info, camd)
-        	if os.path.isfile('/tmp/ecm.info'):
+        	    
+                if os.path.isfile('/tmp/ecm.info'):
         	    fd = open('/tmp/ecm.info')
        		    for line in fd:
                 	print line
@@ -102,7 +169,6 @@ class CamdStart(resource.Resource):
         	self.container.appClosed.append(self.finished)
         	self.actcam = config.NFRSoftcam.actcam.value
         	self.input = input
-        	print 'self.input', self.input
 
     
 	def render_GET(self, req):
@@ -110,13 +176,12 @@ class CamdStart(resource.Resource):
         	req.setHeader('Content-type', 'text/html')
         	req.setHeader('charset', 'UTF-8')
         	html = header_string
-        	print "Infolist:", infoList
         	for info in infoList:
         	    if info == self.input:
         	        self.camstartcmd = Softcam.getcamcmd(self.input)
  			self.activityTimer = eTimer()
 			self.activityTimer.timeout.get().append(self.stopping)
-			self.activityTimer.start(100, False)       	        
+			self.activityTimer.start(1000, False) 
                 	#self.container.execute(info)
                 	html += '<center>%s erfolgreich Gestartet!  <a href="SoftcamControl" target="_self"><input type="submit" value="Zur&uuml;ck"></a></center>' % self.input
                 	continue
@@ -133,6 +198,8 @@ class CamdStart(resource.Resource):
 		self.actcam = config.NFRSoftcam.actcam.value
 		Softcam.stopcam(self.actcam)
 		print "[NFR-SoftCam Manager stop] ", self.actcam
+		cmd = "killall -15 " + self.actcam
+                self.Console.ePopen(cmd)
 		self.actcam = self.input
 		if config.NFRSoftcam.actcam.value != self.actcam: 
 			config.NFRSoftcam.actcam.value = self.actcam
@@ -179,7 +246,7 @@ class CamdKill(resource.Resource):
 
 addExternalChild(('SoftcamControl', SoftcamControl()))
 infoList = checkCams(None)
+ownip = createip(None) 
 for info in infoList:
 	addExternalChild(('%sstart' % info, CamdStart(info)))
 	addExternalChild(('%skill' % info, CamdKill(info)))
-
